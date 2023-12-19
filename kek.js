0c592e2d3dcf76309592baa4dcc07985d31f3f87
AntonShtikel <67754149+AntonShtikel@users.noreply.github.com>
2023-12-20 00:00:15 +0100

    Update kek.js

--- kek.js
+++ kek.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.envConfig = void 0;
-exports.envConfig = { envName: 'ts', test: 'first', kek: '0000000' };
+exports.envConfig = { lol: "kek" };
